768ed50e29936c6314a43798647fdbe8efcdad84
kaaterinam <katyaminina24@gmail.com>
2024-12-26 13:48:49 +0300

    feat: make minesweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,5 @@
 const { NotImplementedError } = require('../extensions/index.js');
+const {min} = require("mocha/lib/reporters");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,9 +24,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix ) {
+  let result=[]
+  for (let i=0; i<matrix.length; i+=1){
+    result[i] = []
+    for (let j=0; j<matrix[i].length; j+=1){
+      if (matrix[i][j]){
+        result[i][j]=1
+      } else{
+        let mines=0
+        for (let x=-1; x<=1; x+=1){
+          for (let y=-1; y<=1; y+=1){
+            if (x ===0 && y===0){
+              continue
+            }
+            let ii=i+x
+            let jj=j+y
+            if (ii>=0 && jj>=0 && ii<matrix.length&& jj<matrix[i].length){
+              if (matrix[ii][jj]){
+                mines+=1
+              }
+            }
+          }
+        }
+        result[i][j]=mines
+      }
+    }
+  }
+  return result
 }
 
 module.exports = {
